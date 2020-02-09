1bcdda9d14ab6a591681b7efb0038c45c4ea07ef
gregorkrz <gregor.krzmanc@gmail.com>
2020-02-09 20:49:28 +0100

    fix mysql connections

--- stats.py
+++ stats.py
@@ -2,21 +2,23 @@ import os
 import mysql.connector
 from urllib.parse import urlparse
 
-error = False
-try:
-    db_conn_string = os.environ.get("CLEARDB_DATABASE_URL", "")
-    parsed_string = urlparse(db_conn_string)
-    mydb = mysql.connector.connect(
-    host=parsed_string.hostname,
-    user=parsed_string.username,
-    passwd=parsed_string.password,
-    database=parsed_string.path.split("/")[1]
-        )
-except mysql.connector.Error as err:
-    print(err)
-    error = True
+def connect():
+    try:
+        db_conn_string = os.environ.get("CLEARDB_DATABASE_URL", "")
+        parsed_string = urlparse(db_conn_string)
+        mydb = mysql.connector.connect(
+        host=parsed_string.hostname,
+        user=parsed_string.username,
+        passwd=parsed_string.password,
+        database=parsed_string.path.split("/")[1]
+            )
+        return mydb, False
+    except mysql.connector.Error as err:
+        print(err)
+        return mydb, True
 
 def getUserStatistics(userID):
+    mydb, error = connect()
     if error: return 0, 0, True
     query = """SELECT total, correct 
     FROM userstatistics 
@@ -27,9 +29,11 @@ def getUserStatistics(userID):
         cur.close()
         return total, correct, False
     cur.close()
+    mydb.disconnect()
     return 0, 0, False
 
 def updateUserStatistics(userID, correct):
+    mydb, error = connect()
     if(correct != 0 and correct != 1): return True
     query = """ insert into userstatistics(userid,total,correct)
                                     values(%s, 1, %s)
@@ -42,4 +46,5 @@ def updateUserStatistics(userID, correct):
     cur.execute(query, (userID, correct, correct,))
     mydb.commit()
     cur.close()
+    mydb.disconnect()
     return False
